9c67d9cc035c79931a9e0a7344b732378fa4ad99
derfc <fredcheung@live.hk>
2021-01-26 18:26:54 +0800

    tue upodate

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@ let contactIcons = document.querySelectorAll(".fab");
 for (let fab of contactIcons) {
 	fab.addEventListener("mouseenter", function () {
 		this.style.transform = "scale(1.4, 1.4)";
-		// fab.css("color", "red !pmoirtant");
+		// $(".fab").css("color", "red");
+
 		this.style.color = "#fdd7a8";
 	});
 	fab.addEventListener("mouseleave", function () {
@@ -14,20 +15,59 @@ for (let fab of contactIcons) {
 		alert("HAHA i didnt link anyhting dont be nosey");
 	});
 }
+// $(".fab").click(function () {
+// 	$(this).css("color", "red");
+// });
 
 // $("#submit").click(function () {
 //   alert("I will get back to you asap, thank you!!");
 // });
 
-// $(".submit").mouseenter(function () {
-//   console.log("hello");
+// $("#submit").mouseenter(function () {
+// 	console.log("hello");
 // });
 
+$(".circle2-html").mouseenter(function () {
+	$(this).append(`<p>HTML</p>`);
+	$(this).css("display", "flex");
+	$(this).css("align-items", "center");
+	$(this).css("justify-content", "center");
+	$(this).css("background", `url()`);
+	$(this).css("font-size", `40px`);
+	// $(".circle2-css").css("background-size", `cover`);
+});
+$(".circle2-html").mouseleave(function () {
+	$(this).empty();
+	$(this).css("background", `url(/asset/html-white.png)`);
+	$(this).css("background-size", `cover`);
+});
+
 $(".circle2-css").mouseenter(function () {
-	console.log("hello");
-	// $(".circle2-css").css("background", "url(/asset/html-white.png)");
+	$(this).append(`<p>CSS</p>`);
+	$(this).css("display", "flex");
+	$(this).css("align-items", "center");
+	$(this).css("justify-content", "center");
+	$(this).css("background", `url()`);
+	$(this).css("font-size", `40px`);
+	// $(".circle2-css").css("background-size", `cover`);
 });
 $(".circle2-css").mouseleave(function () {
-	console.log("on9jai");
-	// $(".circle2-css").css("background", "url(/asset/css-white.png)");
+	$(this).empty();
+	$(this).css("background", `url(/asset/css-white.png)`);
+	$(this).css("background-size", `cover`);
+});
+
+$(".circle2-js").mouseenter(function () {
+	$(this).append(`<p>Javascript</p>`);
+	$(this).css("display", "flex");
+	$(this).css("align-items", "center");
+	$(this).css("justify-content", "center");
+	$(this).css("background", `url()`);
+	$(this).css("font-size", `40px`);
+	// $(".circle2-css").css("background-size", `cover`);
+});
+$(".circle2-js").mouseleave(function () {
+	$(this).empty();
+	$(this).css("background", `url(/asset/js-white.png)`);
+	$(this).css("background-size", `cover`);
 });
